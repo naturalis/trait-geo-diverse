bdc11ada1877da892d14a810b431119842f5ad8a
Rutger Vos <rutgeraldo@gmail.com>
2018-10-19 12:07:01 +0200

    works

--- script/make_occurrences.pl
+++ script/make_occurrences.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use utf8;
 use Getopt::Long;
 use Archive::Zip;
 use File::Temp qw(tempfile);
@@ -42,14 +43,18 @@ $zip->extractMember( 'occurrence.txt' => $tempfile );
 # start reading occurrences and writing output
 open my $in, '<', $tempfile or die $!;
 open my $out, '>', $outfile or die $!;
-my %taxa;
-my @cols;
-while(<in>) {
+my %taxa; # normalized mapping for taxon table
+my @cols; # indexes of all columns of interest
+my ( $idx, $binomialx, $genusx, $speciesx, $rankx ); # indexes of taxon columns
+my @taxon_cols = qw(taxonKey scientificName genus specificEpithet taxonRank); # labels of taxon columns
+while(<$in>) {
 	chomp;
 	my @line = split /\t/, $_;
 	
-	# read header
+	# process header
 	if ( not @cols ) {
+		
+		# collect all column indexes of interest
 		for my $i ( 0 .. $#line ) {
 			if ( exists $columns{$line[$i]} ) {
 				$columns{$line[$i]} = $i;
@@ -57,14 +62,51 @@ while(<in>) {
 			}
 		}
 		
+		# lookup taxon column indexes
+		( $idx, $binomialx, $genusx, $speciesx, $rankx ) = @columns{@taxon_cols};
+		
 		# write filtered header in snake case
 		print $out join( "\t", map { decamelize($_) } sort { $columns{$a} <=> $columns{$b} } keys %columns ), "\n";
 	}
 	
+	# write record, store taxon
 	else {
+		print $out join( "\t", @line[@cols] ), "\n";
 		
+		# lookup values
+		my ( $id, $binomial, $genus, $species, $rank ) = @line[$idx, $binomialx, $genusx, $speciesx, $rankx];
+		
+		# populate data structure
+		$taxa{$id} = {} unless $taxa{$id};
+		$taxa{$id}->{$binomial} = {} unless $taxa{$id}->{$binomial};
+		$taxa{$id}->{$binomial}->{$genus} = {} unless $taxa{$id}->{$binomial}->{$genus};
+		$taxa{$id}->{$binomial}->{$genus}->{$species} = $rank;
 	}
-	
-	
-	
+}
+
+# write normalized taxa table
+open my $tout, '>', $taxafile or die $!;
+print $tout join( "\t", map { decamelize($_) } @taxon_cols ), "\n";
+for my $id ( sort { $a <=> $b } keys %taxa ) {
+	for my $binomial ( sort { $a cmp $b } keys %{ $taxa{$id} } ) {
+		for my $genus ( sort { $a cmp $b }  keys %{ $taxa{$id}->{$binomial} } ) {
+			for my $species ( sort { $a cmp $b } keys %{ $taxa{$id}->{$binomial}->{$genus} } ) {
+				my $rank = $taxa{$id}->{$binomial}->{$genus}->{$species};
+				print $tout join("\t", $id, $binomial, $genus, $species, $rank), "\n";
+			}
+		}
+	}
+}
+
+# converts DarwinCore CamelCase into SQLite-happy snake_case
+sub decamelize {
+    my ($s) = @_;
+    $s =~ s{(\w+)}{
+        ($a = $1) =~ s<(^[A-Z]|(?![a-z])[A-Z])><
+            "_" . lc $1
+        >eg;
+        substr $a, 0;
+    }eg;
+    $s =~ s/gbif_i_d/gbif_id/;
+    $s;
 }
