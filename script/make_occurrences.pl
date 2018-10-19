429f53768aff1ecd5a4579a951969d5815e0d86f
Rutger Vos <rutgeraldo@gmail.com>
2018-10-19 11:24:30 +0200

    updated

--- script/make_occurrences.pl
+++ script/make_occurrences.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use Archive::Zip;
+use File::Temp qw(tempfile);
 
 # process command line arguments
 my $infile;   # zip file downloaded from GBIF
@@ -15,19 +16,55 @@ GetOptions(
 );
 
 # columns to retain
-my @columns = (
-	'gbifID',              # integer (pk)
-	'type',                # text
-	'basisOfRecord',       # text (index)
-	'eventDate',           # text ISO-8601 date
-	'decimalLatitude',     # real
-	'decimalLongitude',    # real
-	'scientificName',      # text, includes naming authority
-	'genus',               # text (index)
-	'specificEpithet',     # text (index)
-	'taxonRank',           # text (index)
-	'datasetKey',          # text (UUID)
-	'elevation',           # real
-	'hasGeospatialIssues', # boolean (true/false), transform to 0/1
-	'taxonKey',            # integer (fk)
+my %columns = (
+	'gbifID'              => undef, # integer (pk)
+	'type'                => undef, # text
+	'basisOfRecord'       => undef, # text (index)
+	'eventDate'           => undef, # text ISO-8601 date
+	'decimalLatitude'     => undef, # real
+	'decimalLongitude'    => undef, # real
+	'scientificName'      => undef, # text, includes naming authority
+	'genus'               => undef, # text (index)
+	'specificEpithet'     => undef, # text (index)
+	'taxonRank'           => undef, # text (index)
+	'datasetKey'          => undef, # text (UUID)
+	'elevation'           => undef, # real
+	'hasGeospatialIssues' => undef, # boolean (true/false), transform to 0/1
+	'taxonKey'            => undef, # integer (fk)
 );
+
+# extract occurrences from archive
+my $zip = Archive::Zip->new($infile);
+my ( $wfh, $tempfile ) = tempfile();
+close $wfh;
+$zip->extractMember( 'occurrence.txt' => $tempfile );
+
+# start reading occurrences and writing output
+open my $in, '<', $tempfile or die $!;
+open my $out, '>', $outfile or die $!;
+my %taxa;
+my @cols;
+while(<in>) {
+	chomp;
+	my @line = split /\t/, $_;
+	
+	# read header
+	if ( not @cols ) {
+		for my $i ( 0 .. $#line ) {
+			if ( exists $columns{$line[$i]} ) {
+				$columns{$line[$i]} = $i;
+				push @cols, $i;
+			}
+		}
+		
+		# write filtered header in snake case
+		print $out join( "\t", map { decamelize($_) } sort { $columns{$a} <=> $columns{$b} } keys %columns ), "\n";
+	}
+	
+	else {
+		
+	}
+	
+	
+	
+}
