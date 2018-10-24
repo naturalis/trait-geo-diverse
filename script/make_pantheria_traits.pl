9e3fe0f410a8828b6df57f3aaf9d1e302f3983c7
Rutger Vos <rutgeraldo@gmail.com>
2018-10-24 15:56:52 +0200

    regenerated, will do TNRS upon ingest

--- script/make_pantheria_traits.pl
+++ script/make_pantheria_traits.pl
@@ -7,25 +7,21 @@ use Scalar::Util 'looks_like_number';
 # process command line arguments
 my $infile;   # PanTHERIA_1-0_WR05_Aug2008.txt
 my $outfile;  # normalized character/state table
-my $taxafile; # normalized taxa table
 my $charfile; # normalized characters
 GetOptions(
 	'infile=s'   => \$infile,
 	'outfile=s'  => \$outfile,
-	'taxafile=s' => \$taxafile,
 	'charfile=s' => \$charfile,
 );
 
 # start reading
-my $binomial_idx = 4;
+my $binomial_idx = 4; # first relevant column, with binomial taxon name
 open my $in,  '<', $infile or die $!;
 open my $out, '>', $outfile or die $!;
 print $out 'pantheria_tax_id', "\t", 'pantheria_char_id', "\t", 'pantheria_char_value', "\n";
 my $flag;
-my $taxid;
-my %taxa;
 while(<$in>) {
-	local $/ = "\r\n";
+	local $/ = "\r\n"; # PanTHERIA data dump has MS-DOS line breaks
 	chomp;
 	my @line = split /\t/, $_;
 	
@@ -43,22 +39,11 @@ while(<$in>) {
 	# write states
 	else {
 		
-		# generate taxon ID
-		my $binomial = $line[$binomial_idx];
-		$taxa{$binomial} = ++$taxid unless $taxa{$binomial};
-		
-		# write state by state
+		# write "taxon - character - state" wherever there is a state (magic number -999.0 is NA)
 		for my $i ( $binomial_idx + 1 .. $#line ) {
 			if ( $line[$i] =~ /;/ or ( looks_like_number($line[$i]) && $line[$i] != -999.00 ) ) {
-				print $out $taxa{$binomial}, "\t", ( $i - $binomial_idx ), "\t", $line[$i], "\n";
+				print $out $line[$binomial_idx], "\t", ( $i - $binomial_idx ), "\t", $line[$i], "\n";
 			}
 		}
 	}
-}
-
-# write taxa table
-open my $tout, '>', $taxafile or die $!;
-print $tout 'pantheria_tax_id', "\t", 'pantheria_tax_label', "\n";
-for my $binomial ( sort { $taxa{$a} <=> $taxa{$b} } keys %taxa ) {
-	print $tout $taxa{$binomial}, "\t", $binomial, "\n";
 }
