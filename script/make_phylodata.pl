2f1fd8e6ef19b0d47ab2c04fe54d9c44c77ce3db
Rutger Vos <rutgeraldo@gmail.com>
2018-10-24 19:46:04 +0200

    reran without separate taxon table

--- script/make_phylodata.pl
+++ script/make_phylodata.pl
@@ -5,15 +5,13 @@ use Getopt::Long;
 use Bio::Phylo::IO qw'parse_tree';
 
 # process command line arguments
-my $infile;   # tree file
-my $format;   # newick | nexus | nexml, etc.
-my $outfile;  # tab-separated table with tree topology
-my $taxafile; # tab separated table with taxon mapping
+my $infile;  # tree file
+my $format;  # newick | nexus | nexml, etc.
+my $outfile; # tab-separated table with tree topology
 GetOptions(
 	'infile=s'   => \$infile,
 	'format=s'   => \$format,
 	'outfile=s'  => \$outfile,
-	'taxafile=s' => \$taxafile,
 );
 
 # read tree object
@@ -22,53 +20,34 @@ my $tree = parse_tree(
 	'-file'   => $infile,
 );
 
-# create taxon IDs
-my %taxa;
-my $taxonid;
-for my $tip ( @{ $tree->get_terminals } ) {
-	my $name = $tip->get_name;
+# remove underscores from taxon labels
+$tree->visit(sub{
+	my $node = shift;
+	my $name = $node->get_name;
 	$name =~ s/_/ /g;
-	$taxa{$name} = ++$taxonid;
-	$tip->set_name($taxonid);
-}
-
-# write taxa table
-{
-	open my $out, '>', $taxafile or die $!;
-	print $out 'taxon_id', "\t", 'taxon_name', "\t", 'tree_name', "\n";
-	for my $name ( sort { $taxa{$a} <=> $taxa{$b} } keys %taxa ) {
-		print $out $taxa{$name}, "\t", $name, "\t", $infile, "\n";
-	}
-	close $out;
-}
+	$node->set_name($name);
+});
 
 # write tree table
-{
-	open my $out, '>', $outfile or die $!;
-	print $out 'node_id', "\t", 'parent_id', "\t", 'branch_length', "\t", 'taxon_id', "\t", 'tree_name', "\n";
-	$tree->visit_depth_first(
-		'-pre' => sub {
-			my $node = shift;
-			
-			# node is not root
-			my $parent_id = '';
-			if ( my $parent = $node->get_parent ) {
-				$parent_id = $parent->get_id;
-			}
-			
-			# node is tip
-			my $tip_label = '';
-			if ( $node->is_terminal ) {
-				$tip_label = $node->get_name;
-			}
-			
-			# node has branch length
-			my $length = '';
-			$length = $node->get_branch_length;
-			
-			# write output
-			print $out $node->get_id, "\t", $parent_id, "\t", $length, "\t", $tip_label, "\t", $infile, "\n";
+open my $out, '>', $outfile or die $!;
+print $out 'node_id', "\t", 'parent_id', "\t", 'branch_length', "\t", 'taxon', "\t", 'tree_name', "\n";
+$tree->visit_depth_first(
+	'-pre' => sub {
+		my $node = shift;
+		
+		# node is not root
+		my $parent_id = '';
+		if ( my $parent = $node->get_parent ) {
+			$parent_id = $parent->get_id;
 		}
-	);
-	close $out;
-}
+			
+		# node is tip
+		my $name = $node->get_name || '';
+			
+		# node has branch length
+		my $length = $node->get_branch_length || '';
+			
+		# write output
+		print $out $node->get_id, "\t", $parent_id, "\t", $length, "\t", $name, "\t", $infile, "\n";
+	}
+);
