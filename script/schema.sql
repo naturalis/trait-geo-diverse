5bdc79ade8e8c13cf1059b3a0f1565c46c6df524
Rutger Vos <rutgeraldo@gmail.com>
2018-10-24 23:13:18 +0200

    updated

--- script/schema.sql
+++ script/schema.sql
@@ -1,6 +1,28 @@
-create table if not exists taxa (
+create table if not exists taxa(
 	taxon_id integer constraint taxon_pk primary key asc autoincrement,
-	taxon_name text -- index
+	taxon_name text, -- index
+	taxon_level text,
+	msw_id integer,
+	tropicos_id integer,
+	kingdom integer,
+	subkingdom integer,
+	infrakingdom integer,
+	superdivision integer,
+	division integer,
+	subdivision integer,
+	class integer,
+	subclass integer,
+	order integer,
+	suborder integer,
+	infraorder integer,
+	superfamily integer,
+	family integer,
+	subfamily integer,
+	tribe integer,
+	genus integer,
+	subgenus integer,
+	species integer,
+	subspecies integer
 );
 
 create table if not exists trees (
@@ -8,7 +30,7 @@ create table if not exists trees (
 	node_id integer, -- index
 	parent_id integer, -- index
 	taxon_id integer constraint taxon_fk references taxa (taxon_id), -- index
-	taxon_name text,        
+	label text,        
 	branch_length real,
 	tree_name text -- index
 );
@@ -20,10 +42,10 @@ create table if not exists occurrences (
 	event_date text, -- ISO-8601 date
 	decimal_latitude real,
 	decimal_longitude real,
-	scientific_name text, -- index
-	genus text, -- index
-	specific_epithet text, -- index
-	taxon_rank text, -- index	
+	label text, -- index scientific_name
+--	genus text, -- index
+--	specific_epithet text, -- index
+--	taxon_rank text, -- index	
 	dataset_key text, -- index
 	elevation real,
 	has_geospatial_issues integer, -- index, boolean (true/false), transform to 0/1
