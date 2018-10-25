01010cd4f580507db0cfb1711d8ed3c7e33118e8
Rutger Vos <rutgeraldo@gmail.com>
2018-10-25 23:17:14 +0200

    fml

--- script/load_gbif_nub.pl
+++ script/load_gbif_nub.pl
@@ -22,9 +22,7 @@ GetOptions(
 
 # instantiate objects
 my $schema    = MY::Schema->connect("dbi:SQLite:$db");
-my $branch_rs = $schema->resultset('Branch');
 my $taxon_rs  = $schema->resultset('Taxa');
-my $tree_id   = $schema->resultset('Tree')->create( { tree_name => $infile } )->tree_id;
 my $zip       = Archive::Zip->new($infile);
 
 # extract Taxon.tsv table
@@ -33,7 +31,7 @@ close $wfh;
 $zip->extractMember( 'Taxon.tsv' => $tempfile );
 
 # start reading the file
-my ( @header, %tree );
+my @header;
 open my $fh, '<', $tempfile or die $!;
 while(<$fh>) {
 	chomp;
@@ -55,55 +53,12 @@ while(<$fh>) {
 		
 		# check if focal class, skip otherwise
 		if ( $record{'class'} and $record{'class'} eq $class ) {
-			my $nesting = \%tree;
-			my @cols = qw(class order family genericName specificEpithet infraspecificEpithet);
-
-			# traverse classification from higher to lower taxon, growing the data structure
-			for my $level ( @cols ) {
-				my $name = $record{$level};
-				if ( $name and $name =~ /\S/ ) {
-					$nesting->{$name} = {} unless $nesting->{$name};
-					$nesting = $nesting->{$name};
-				}
-			}
-			
-			# traverse classification from lower to higher
-			LEVEL: for my $level ( reverse @cols ) {
-				my $name = $record{$level};
-				if ( $name and $name =~ /\S/ ) {
-					$nesting->{'insert_object'} = {
-						'taxon_name'     => $record{'canonicalName'},						
-						'gbif_taxon_key' => $record{'taxonID'},
-						'taxon_level'    => uc($record{'taxonRank'}),
-					};
-					last LEVEL;
-				}
-			}
+			$taxon_rs->create({
+				'taxon_name'     => $record{'canonicalName'},						
+				'gbif_taxon_key' => $record{'taxonID'},
+				'taxon_level'    => uc($record{'taxonRank'}),				
+			});
 		}
-	}
-}
-print Dumper(\%tree);
-traverse(\%tree);
-sub traverse {
-	my ( $hoh, $parent_id ) = @_;
-	if ( ref($hoh) ) {
-		
-		# prepare $insert_object to put it in Taxa table
-		my $insert_object = delete $hoh->{'insert_object'};
-		my $taxon = $taxon_rs->create($insert_object);
-		
-		# create branch
-		$branch_rs->create({
-			'tree_id'   => $tree_id,			
-			'parent_id' => $parent_id,
-			'taxon_id'  => $taxon->taxon_id,			
-			'node_id'   => $insert_object->{'gbif_taxon_key'},
-			'label'     => $insert_object->{'taxon_name'},
-		});
-		warn Dumper($insert_object);
-		
-		# traverse deeper
-		traverse( $_, $insert_object->{'gbif_taxon_key'} ) for values %$hoh;
 	}
 }
 
