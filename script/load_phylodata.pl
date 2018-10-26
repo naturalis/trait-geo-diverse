aa63c6ee24299ad46fb6cee90a9be663cc0ca5b5
Rutger Vos <rutgeraldo@gmail.com>
2018-10-26 16:40:18 +0200

    now first does a local db check before conservative TNRS

--- script/load_phylodata.pl
+++ script/load_phylodata.pl
@@ -3,10 +3,12 @@ use strict;
 use warnings;
 use Getopt::Long;
 use MY::Schema;
+use Log::Log4perl qw(:easy);
 use MY::Taxize qw[gnr_resolve TRUE FALSE];
+Log::Log4perl->easy_init($DEBUG);
 
 # process command line arguments
-my $db = '/Users/rutger.vos/Dropbox/documents/projects/dropbox-projects/trait-geo-diverse/tgd.db';
+my $db = $ENV{'HOME'} . '/Dropbox/documents/projects/dropbox-projects/trait-geo-diverse/tgd.db';
 my $infile;
 my $taxonomy;
 GetOptions(
@@ -54,25 +56,44 @@ while(<$fh>) {
 		# update fields
 		$record{tree_id} = $tree_id;
 		if ( $taxonomy and $record{label} ) {
-			
-			# do a global names lookup
-			my $results = gnr_resolve( 
-				'names'           => [ $record{label} ], 
-				'data_source_ids' => [ $data_source_id ],
-				'canonical'       => TRUE,
-				'best_match_only' => TRUE,
-				'fields'          => [ "all" ]
-			);
-			
-			# find the ID in the local database and look up the primary key
-			if ( $results->[0] ) {
-			    my $local_id = $results->[0]->{'local_id'};
-			    $record{taxon_id} = $taxon_rs->single({ $colname => $local_id })->taxon_id;
-			    warn $record{label}, "\t", $results->[0]->{matched_name2}, "\n";
-			}			
+			$record{taxon_id} = get_taxon_id( $record{label} );
 		}
 		
 		# create the branch
 		$branch_rs->create(\%record);
 	}
+}
+
+sub get_taxon_id {
+	my $label = shift;
+	my $taxon_id;
+	
+	# do local query
+	if ( my $taxon = $taxon_rs->single({ 'taxon_name' => $label }) ) {
+		$taxon_id = $taxon->taxon_id;
+		DEBUG "Exact match in local database for '$label' => $taxon_id";
+	}
+	
+	# do tnrs
+	else {
+		my $results = gnr_resolve( 
+			'names'           => [ $label ], 
+			'data_source_ids' => [ $data_source_id ],
+			'canonical'       => TRUE,
+			'best_match_only' => TRUE,
+			'fields'          => [ "all" ]
+		);	
+		if ( $results->[0] ) {
+			my $match = $results->[0]->{'matched_name2'};
+			my $score = $results->[0]->{'score'};			
+			if ( $score >= 0.95 ) {
+				$taxon_id = $taxon_rs->single({ $colname => $local_id })->taxon_id;
+				DEBUG "TNRS match for '$label' => '$match' ($taxon_id)";
+			}
+			else {
+				DEBUG "TNRS matching score for '$label' => '$match' not high enough ($score)";
+			}
+		}		
+	}
+	return $taxon_id;
 }
