382ed9adeacc30ba2f96b6c10c5d868c1ab35b85
Rutger Vos <rutgeraldo@gmail.com>
2019-01-25 17:05:58 +0100

    completed workflow

--- script/extract_occurrences.pl
+++ script/extract_occurrences.pl
@@ -5,11 +5,13 @@ use Getopt::Long;
 use MY::Schema;
 use MY::OccurrenceFilter;
 use FindBin qw($Bin);
+use Log::Log4perl qw(:easy);
 
 # these are the constructor arguments for the occurrence filter
 my %cargs = (
 	'outdir'   => undef,                        # where to write occurrences, per species, as CSV
 	'dbfile'   => $Bin . '/../data/sql/tgd.db', # location of the sqlite database
+	'shpfile'  => undef,                        # location of the shape file
 	'mindate'  => undef,                        # minimum eventDate, in ISO8601 format
 	'maxdate'  => undef,                        # maximum ...
 	'subsp'    => 1,                            # whether to expand subspecies
@@ -30,13 +32,20 @@ my %cargs = (
 my %args = map { $_ . '=s' => ref($cargs{$_}) ? $cargs{$_} : \$cargs{$_} } keys %cargs;
 
 # the top level taxa that we will expand to species level
-my @taxa = qw(PERISSODACTYLA ARTIODACTYLA);
+my @taxa;
 
 # where to write CSV files
 my $outdir;
 
+# verbose level: warn
+my $verbosity = 3;
+my $verbose = 0;
+
 # process command line arguments
-GetOptions(%args, 'taxa=s' => \@taxa, 'outdir=s' => \$outdir);
+GetOptions(%args, 'taxa=s' => \@taxa, 'outdir=s' => \$outdir, 'verbose+' => \$verbose );
+
+# instantiate logger
+Log::Log4perl->easy_init( ($verbosity-$verbose) * 10_000 );
 
 # instantiate objects
 my $db     = MY::Schema->connect( 'dbi:SQLite:' . $cargs{'dbfile'} );
@@ -53,8 +62,8 @@ for my $taxon ( @taxa ) {
 }
 sub _recurse {		
 	my ( $node, $species ) = @_;
-	if ( $node->taxonvariant->taxa->taxon_level eq 'SPECIES' ) {
-		push @$species, $node->taxonvariant->taxa;		
+	if ( $node->taxonvariant->taxon->taxon_level eq 'SPECIES' ) {
+		push @$species, $node->taxonvariant->taxon;		
 	}
 	else {
 		my $rs = $db->resultset('Branch')->search({
@@ -66,10 +75,31 @@ sub _recurse {
 		}
 	}
 }
+INFO "Expanded @taxa into ".scalar(@species)." species";
 
 # iterate over species
-for my $sp ( @species ) {
-	my @records = $filter->get_occurrences_for_species($sp);
-	@records = $filter->filter_occurrences_by_shapes( $sp => @records );
-	@records = $filter->filter_occurrences_by_distances(@records);
+SPECIES: for my $sp ( @species ) {
+ 	my @records = $filter->get_occurrences_for_species($sp);
+ 	next SPECIES if scalar(@records) < 10;
+ 	@records = $filter->filter_occurrences_by_shapes( $sp => @records );
+ 	next SPECIES if scalar(@records) < 10;
+ 	@records = $filter->filter_occurrences_by_distances(@records);
+ 	next SPECIES if scalar(@records) < 10;
+
+	# write CSV
+ 	my $filename = $sp->taxon_name;
+ 	$filename =~ s/ /_/g;
+ 	$filename .= '.csv';
+ 	$filename = $outdir . '/' . $filename;
+	open my $fh, '>', $filename or die $!;
+	print $fh join(",", qw(gbif_id taxon_name decimal_latitude decimal_longitude)), "\n";
+	for my $r ( @records ) {
+		my @values = (
+			$r->gbif_id,
+			$r->taxonvariant->taxon->taxon_name,
+			$r->decimal_latitude,
+			$r->decimal_longitude
+		);
+		print $fh join(",", @values), "\n";
+	}
 }
--- script/extract_occurrences.sh
+++ script/extract_occurrences.sh
@@ -3,29 +3,32 @@
 # this script demonstrates how to extract filtered occurrences:
 
 perl -I../lib extract_occurrences.pl \
-	--outdir=../../trait-geo-diverse-ungulates/data \
+	--outdir=../../trait-geo-diverse-ungulates/data/occurrences \
 	--dbfile=../data/sql/tgd.db \
+	--shpfile=$HOME/Dropbox/documents/projects/dropbox-projects/trait-geo-diverse-ungulates/Input_Datasets/Range_Data/TERRESTRIAL_MAMMALS/TERRESTRIAL_MAMMALS \
 	--subsp=1 \
 	--taxa=PERISSODACTYLA \
 	--taxa=ARTIODACTYLA \
-	--thresh=2 \
-#	--mindate=1970-01-01 \
-#	--maxdate=2018-12-31 \
+	--thresh=4 \
+	--mindate=1900-01-01 \
+	--maxdate=2018-12-31 \
 	--basis=PRESERVED_SPECIMEN \
 	--basis=HUMAN_OBSERVATION \
 	--basis=MACHINE_OBSERVATION \
 	--basis=FOSSIL_SPECIMEN \
 	--basis=OBSERVATION \
 	--basis=LITERATURE \
-	--basis=MATERIAL_SAMPLE
+	--basis=MATERIAL_SAMPLE \
+	--verbose
 
 # explanation of options and arguments:
 # --outdir  - where to write CSV files with filtered occurrences
 # --dbfile  - location of the database file
+# --shpfile - location of the shapefile, without extension (!)
 # --subsp   - whether to include occurrences of subspecies
 # --taxa    - which higher taxa (sensu MSW3) to expand to species level
 # --thresh  - multiplier of the stdev, for filtering outlying occurrences
-# --mindate - occurrences onwards from this date are included
-# --maxdate - occurrences up till this date are included
+# --mindate - occurrences onwards from this date are included, yyyy-mm-dd
+# --maxdate - occurrences up till this date are included, yyyy-mm-dd
 # --basis   - occurrences with this basis_of_record are included
-
+# --verbose - provide more feedback
